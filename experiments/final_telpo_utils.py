b1b1d84f4d2518ee67d3571a149f43ac077a165a
hannahaih <nyxfirenyx@gmail.com>
2021-05-25 23:27:06 +0200

    updating main nb

--- experiments/final_telpo_utils.py
+++ experiments/final_telpo_utils.py
@@ -1,6 +1,6 @@
-########################################
-# circuit structuring funcs
-########################################
+################################################################################
+# structuring funcs
+################################################################################
 
 # func to get qubit inums of upper and lower half of the circ 
 def fst_n_sec_half_nums(qnum):
@@ -32,9 +32,53 @@ def get_unitary_pairs(qnum):
 
     return upper_pairs, lower_pairs
 
-########################################
+# func to get dict with all lists plus reversed lists for qubit inum pairings, one-q-gates and two-q-gates
+def get_all_gatesets_and_pairing_lists(n, one_q_gates, two_q_gates):
+    '''args: 
+        n (int), 
+        one_q_gates, two_q_gates (lists storing gate objects)
+    
+    returns: 
+    fst_2gate_pairs, rev_fst_2gate_pairs, sec_2gate_pairs, rev_sec_2gate_pairs, one_q_gates, rev_one_q_gates, two_q_gates, rev_two_q_gates
+    '''
+    qnum = 2*n +1 # num of qubits in circ
+    
+    # get pairings of qubit inmus for both halfs of circ
+    fst_2gate_pairs, sec_2gate_pairs = get_unitary_pairs(qnum)
+    
+    # lists in reversed order for all items but the first one
+    rev_fst_2gate_pairs = [fst_2gate_pairs[0]] + [x for x in reversed(fst_2gate_pairs[1:])]
+    rev_sec_2gate_pairs = [sec_2gate_pairs[0]] + [x for x in reversed(sec_2gate_pairs[1:])]
+    rev_two_q_gates =  [two_q_gates[0]] + [x for x in reversed(two_q_gates[1:])]
+    rev_one_q_gates = [one_q_gates[0]] + [x for x in reversed(one_q_gates[1:])]
+
+    # repeat gates if selection in list is smaller than num of pairs in unitary
+    if len(two_q_gates) < len(fst_2gate_pairs):
+        two_q_gates = (1+int(len(fst_2gate_pairs)/len(two_q_gates)))*two_q_gates
+    if len(one_q_gates) < n*2:
+        one_q_gates = (1+int(n*2/len(one_q_gates)))*one_q_gates 
+    if len(rev_two_q_gates) < len(rev_fst_2gate_pairs):
+        rev_two_q_gates = (1+int(len(rev_fst_2gate_pairs)/len(rev_two_q_gates)))*rev_two_q_gates
+    if len(rev_one_q_gates) < n*2:
+        rev_one_q_gates = (1+int(n*2/len(rev_one_q_gates)))*rev_one_q_gates
+    
+    # cut gate lists to len of pairing lists / num of qubits if they are longer
+    two_q_gates = [two_q_gates[i] for i in range(len(fst_2gate_pairs))]
+    one_q_gates = [one_q_gates[i] for i in range(n*2)]
+    rev_two_q_gates = [rev_two_q_gates[i] for i in range(len(rev_fst_2gate_pairs))]
+    rev_one_q_gates = [rev_one_q_gates[i] for i in range(n*2)]
+    
+    # storing all in list dict lsd
+    lsd = {"upper_pairs": fst_2gate_pairs,"upper_pairs_rev": rev_fst_2gate_pairs, 
+         "lower_pairs":sec_2gate_pairs, "lower_pairs_rev": rev_sec_2gate_pairs, 
+         "one_q_gates":one_q_gates,"one_q_gates_rev":rev_one_q_gates, 
+         "two_q_gates":two_q_gates, "two_q_gates_rev":rev_two_q_gates}
+    
+    return lsd
+
+################################################################################
 # circuit operation funcs
-########################################
+################################################################################
 
 # funcs for bell pair entanglement, args: circuit, nested list of qubit inums
 def entangle_bell_pairs(circ, bell_pairs):  
@@ -52,3 +96,48 @@ def apply_bob_gates(circ,ibob,measpair):
     measpair = sorted(measpair)
     circ.cz(measpair[0],ibob)
     circ.cx(measpair[1],ibob)
+    
+################################################################################
+# funcs for general telpo protocol to test scrambling properties
+################################################################################
+
+# func to apply method "gateset" to circ, args: circ and initial lists with gates
+
+def apply_gate_set_test(circ, two_q_gates, one_q_gates):
+    
+    # get n (unitary size) from qubit num
+    n = int((len(circ.qubits)-1)/2) 
+    
+    # get list dict
+    lsd = get_all_gatesets_and_pairing_lists(n, one_q_gates, two_q_gates)
+    
+    # and all the lists
+    fst_2gate_pairs, rev_fst_2gate_pairs = lsd.get("upper_pairs"), lsd.get("upper_pairs_rev")
+    sec_2gate_pairs, rev_sec_2gate_pairs = lsd.get("lower_pairs"), lsd.get("lower_pairs_rev")
+    one_q_gates, rev_one_q_gates = lsd.get("one_q_gates") , lsd.get("one_q_gates_rev")
+    two_q_gates, rev_two_q_gates = lsd.get("two_q_gates"), lsd.get("two_q_gates_rev")
+
+    # apply two qubit gates for pairs in fist half of circ
+    for i in range(len(fst_2gate_pairs)):
+        circ.append(two_q_gates[i],fst_2gate_pairs[i])
+
+    # apply two qubit gates for pairs in second half of circ
+    for i in range(len(sec_2gate_pairs)):
+        circ.append(two_q_gates[i],sec_2gate_pairs[i])
+
+    # apply single qubit gates for all but bob qubit
+    for i in range(2*n):
+        circ.append(one_q_gates[i],[i])
+
+    # apply two qubit gates for reversed pairs in fist half of circ
+    for i in range(len(rev_fst_2gate_pairs)):
+        circ.append(rev_two_q_gates[i],rev_fst_2gate_pairs[i])
+
+    # apply two qubit gates for reversed pairs in second half of circ
+    for i in range(len(rev_sec_2gate_pairs)):
+        circ.append(rev_two_q_gates[i],rev_sec_2gate_pairs[i])
+        
+        
+def apply_unitary_test(circ, unitary):
+    print("unitary method coming soon")
+    print('\n\tsorry, not implemented yet')
